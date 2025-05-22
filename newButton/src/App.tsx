dbdb73fac135ee5192aaa9014d086f058f25bd75
PranavRasal <pranavcode19@gmail.com>
2025-05-22 20:52:24 +0530

    feat: initialize new React project with Vite and TypeScript
    - Added package.json with dependencies for React, Vite, and TypeScript.
    - Included ESLint configuration for code linting.
    - Created initial application structure with App component and styling.
    - Added SVG assets for React and Vite logos.
    - Configured TypeScript with strict settings and bundler mode.
    - Set up Vite configuration for React plugin.
    - Established basic CSS styles for the application layout and components.

---
+++ newButton/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() { 
+
+
+const [conter , setconster]= useState(2)
+
+
+const decrease = () => {
+if (conter > 0 ){
+setconster(conter - 1)
+}}
+
+const increase = () => {
+if(conter < 5){
+ setconster(conter +1)
+}}
+
+
+  return (
+    <>
+      <h1>age {conter}</h1>
+    <button onClick={increase}>increase {conter}</button><br />
+      <button onClick={decrease}>decrease {conter}</button>
+    </>
+  )
+}
+
+export default App
